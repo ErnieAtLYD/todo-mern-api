5b1c76d67da6625fe9a2a35fd4026bf0fb8f18b3
Ernie Hsiung <ernie@wyncode.co>
2020-09-08 17:12:16 -0400

    change root

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.use('/api/tasks', taskRouter);
 
 if (process.env.NODE_ENV === 'production') {
   // Handle React routing, return all requests to React app
-  app.get('*', (request, response) => {
+  app.get('/*', (request, response) => {
     response.sendFile(
       path.resolve(__dirname, '..', 'client', 'build', 'index.html')
     );
